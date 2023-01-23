70d04bd8a1fdfa0063e4fbdfd540c4879fe76730
vincent_bottini <vincent_bottini@insideranken.org>
2023-01-23 13:38:32 -0600

    HOT1 Part 1 Complete

---
+++ HOT1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using HOT1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HOT1.Controllers {
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.DistanceConverted = 0;
+            ViewBag.DistanceInInches = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(DistanceConverterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.DistanceConverted = model.InchesToCM();
+                ViewBag.DistanceInInches = model.DistanceInInches;
+            } 
+            else
+            {
+                ViewBag.DistanceConverted = 0;
+                ViewBag.DistanceInInches = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ HOT1/Models/DistanceConverterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HOT1.Models
+{
+    public class DistanceConverterModel
+    {
+        public const decimal CM_PER_IN = 2.54M;
+
+        [Required(ErrorMessage = "Please enter a distance in inches")]
+        [Range(1, 500, ErrorMessage = "Please enter a value between 1 and 500.")]
+        public decimal? DistanceInInches { get; set; }
+        public decimal? DistanceInCentimeters { get; set; }
+
+        public decimal? InchesToCM()
+        {
+            decimal? DistanceConverted = DistanceInInches * CM_PER_IN;
+            return DistanceConverted;
+        }
+    }
+}
