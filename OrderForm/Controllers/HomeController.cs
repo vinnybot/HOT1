d8372595f3eb52d17f94573d0d5fcfdad70b1abf
vincent_bottini <vincent_bottini@insideranken.org>
2023-01-23 15:35:37 -0600

    HOT1 Complete

---
+++ OrderForm/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using OrderForm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(OrderFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Total = model.CalculateTotal();
+                ViewBag.Qty = model.Qty;
+                ViewBag.Tax = model.CalculateTax();
+                ViewBag.SubTotal = model.CalculateSubTotal();
+                ViewBag.PricePerShirt = model.PricePerShirt;
+                ViewBag.DiscountMessage = model.DiscountMessage;
+            }
+            else
+            {
+                ViewBag.Total = 0;
+                ViewBag.Qty = 0;
+                ViewBag.Tax = 0;
+                ViewBag.SubTotal = 0;
+                ViewBag.PricePerShirt = 0;
+                ViewBag.DiscountMessage = "";
+            }
+            return View(model);
+        }
+    }
+}
---
+++ OrderForm/Models/OrderFormModel.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderForm.Models
+{
+    public class OrderFormModel
+    {
+        [Required(ErrorMessage = "Please enter a quantity bro")]
+        public int? Qty { get; set; }
+        public string? DiscountCode { get; set; }
+
+        public string? DiscountMessage { get; set; }
+
+        public decimal? PricePerShirt { get; set; } = 15;
+
+        public decimal? Subtotal { get; set; }
+
+        public decimal? Tax { get; set; } = 0.08M;
+        public decimal? Total { get; set; }
+
+        public decimal? CalculateTotal()
+        {
+            decimal? subTotal = 0;
+
+            decimal? tax = 0;
+
+            decimal? total = 0;
+
+            if (DiscountCode == "1390")
+            {
+                PricePerShirt = 12;
+                DiscountMessage = "20% Discount Applied";
+
+                subTotal = PricePerShirt * Qty;
+
+                tax = subTotal * Tax;
+
+                total = subTotal + tax;
+            }
+
+            if (DiscountCode == "6175")
+            {
+                PricePerShirt = 10.50M;
+                DiscountMessage = "30% Discount Applied";
+
+                subTotal = PricePerShirt * Qty;
+
+                tax = subTotal * Tax;
+
+                total = subTotal + tax;
+            }
+
+            if (DiscountCode == "BB88")
+            {
+                PricePerShirt = 13.50M;
+                DiscountMessage = "10% Discount Applied";
+
+                subTotal = PricePerShirt * Qty;
+
+                tax = subTotal * Tax;
+
+                total = subTotal + tax;
+            }
+
+            else
+            {
+                subTotal = PricePerShirt * Qty;
+
+                tax = subTotal * Tax;
+
+                total = subTotal + tax;
+            }
+
+            return total;
+        }
+        public decimal? CalculateTax()
+        {
+            decimal? subTotal = PricePerShirt * Qty;
+            decimal? tax = subTotal * Tax;
+
+            return tax;
+        }
+        public decimal? CalculateSubTotal()
+        {
+            decimal? subTotal = PricePerShirt * Qty;
+
+            return subTotal;
+        }
+    }
+}
